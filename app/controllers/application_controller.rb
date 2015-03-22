2a86fbb80a3a88e95e94662dd3d1e96039888b72
drake laptop <drake@hungrychild.org>
2015-03-22 10:02:11 -1000

    view overhaul

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :load_user
+  
+  
+  def load_user
+    @user = params[:u] || session[:u] || "drake@seemail.cc"
+    
+    session[:u] = params[:u] if session[:u] != params[:u] and !params[:u].nil?
+  end
+  
 end
