d8b8e9e3dabd537d2f8ae4acdfa4ae7409e4bf15
drake laptop <drake@hungrychild.org>
2015-03-23 18:19:23 -1000

    cleaner flow

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -57,7 +57,7 @@ class EmailsController < ApplicationController
 
     respond_to do |format|
       if @email.save
-        format.html { redirect_to @email, notice: 'Email was successfully created.' }
+        format.html { redirect_to @email, notice: 'Email was sent successfully.' }
         format.json { render json: @email, status: :created, location: @email }
       else
         format.html { render action: "new" }
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  
+  
+  def nice_date(date)
+    return "" if date.nil?
+    #time_ago_in_words(date) + " " + (date.past? ? "ago" : "from now")
+    date_displayed = date.strftime "%b %e"
+    date_displayed += date.strftime " %Y" if date.year != Time.now.year
+    date_displayed
+  end
+  def nice_time(date)
+    return "" if date.nil?
+    date.strftime "%R"
+  end
+  def nice_date_and_time(date)
+    nice_date(date) + " " + nice_time(date)
+  end
+  
 end
